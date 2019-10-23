685a8ee3788fe907c2268820d94ea3efd36ef8e1
anaribeiros <anaribeiro3105@gmail.com>
2019-10-23 08:20:22 -0300

    proibindo adição de rota sem ponto de referência ou sem bairro

--- Ecard/CadastroRotaEdit.aspx.cs
+++ Ecard/CadastroRotaEdit.aspx.cs
@@ -13,23 +13,27 @@ namespace Ecard
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            DALBairro dal_bairro = new DALBairro();
-            ListItem li;
-            foreach (Bairro b in dal_bairro.SelectAll())
+            if (!IsPostBack)
             {
-                li = new ListItem();
-                li.Text = b.Nome;
-                li.Value = (b.Id).ToString();
-                CheckBoxListBairro.Items.Add(li);
+                DALBairro dal_bairro = new DALBairro();
+                ListItem li;
+                foreach (Bairro b in dal_bairro.SelectAll())
+                {
+                    li = new ListItem();
+                    li.Text = b.Nome;
+                    li.Value = (b.Id).ToString();
+                    CheckBoxListBairro.Items.Add(li);
+                }
+                DALPonto_referencia dal_referencia = new DALPonto_referencia();
+                foreach (Ponto_referencia p in dal_referencia.SelectAll())
+                {
+                    li = new ListItem();
+                    li.Text = p.Nome;
+                    li.Value = (p.Id).ToString();
+                    CheckBoxListReferencia.Items.Add(li);
+                }
             }
-            DALPonto_referencia dal_referencia = new DALPonto_referencia();
-            foreach (Ponto_referencia p in dal_referencia.SelectAll())
-            {
-                li = new ListItem();
-                li.Text = p.Nome;
-                li.Value = (p.Id).ToString();
-                CheckBoxListReferencia.Items.Add(li);
-            }
+            
         }
         protected void Unnamed2_Click(object sender, EventArgs e)
         {
@@ -52,11 +56,17 @@ namespace Ecard
                     referencias.Add(int.Parse(item.Value.ToString()));
                 }
             }
-            if (referencias.Count != 0 || bairros.Count != 0)
+            if (referencias.Count != 0 && bairros.Count != 0)
             {
                 d.Insert(rota, referencias, bairros);
+                Response.Redirect("~//CadastroRota.aspx");
             }
-            Response.Redirect("~//CadastroRota.aspx");
+            else
+            {
+                if (bairros.Count == 0) CustomValidator1.IsValid = false;
+                if (referencias.Count == 0) CustomValidator2.IsValid = false;
+            }
+            
         }
 
     }
